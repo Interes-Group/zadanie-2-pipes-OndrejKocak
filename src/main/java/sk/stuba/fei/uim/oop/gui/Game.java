f49fc0e150f3512331e876d30d1766644b640f07
kocak <streax14@gmail.com>
2023-04-02 10:31:54 +0200

    new frame

---
+++ src/main/java/sk/stuba/fei/uim/oop/gui/Game.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.gui;
+
+import javax.swing.*;
+
+public class Game {
+    public Game() {
+        JFrame frame = new JFrame("Water Pipes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1280, 720);
+
+        frame.setVisible(true);
+    }
+}
